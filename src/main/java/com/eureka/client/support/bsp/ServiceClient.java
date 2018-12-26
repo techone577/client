5baada15c1e71bd5041512f8c24e79de6bd847b0
techoneduan <techoneduan@blackfish.cn>
2018-12-26 14:48:07 +0800

    next -> 客户端负载均衡、超时熔断、框架整合

--- src/main/java/com/eureka/client/support/bsp/ServiceClient.java
+++ src/main/java/com/eureka/client/support/bsp/ServiceClient.java
@@ -1,13 +1,17 @@
 package com.eureka.client.support.bsp;
 
+import com.eureka.client.model.constant.Constants;
 import com.eureka.client.model.constant.NettyHeader;
 import com.eureka.client.model.constant.RedisConstants;
+import com.eureka.client.model.constant.RestMethod;
 import com.eureka.client.model.entity.NettyReqEntity;
 import com.eureka.client.model.entity.ServiceEntity;
 import com.eureka.client.model.enums.ErrorCodeEnum;
 import com.eureka.client.model.syncMap.SyncMap;
 import com.eureka.client.netty.NettyClient;
 import com.eureka.client.support.exception.UnifiedException;
+import com.eureka.client.support.interceptor.RestRequestInterceptor;
+import com.eureka.client.support.utils.Base64Util;
 import com.eureka.client.support.utils.JsonUtil;
 import com.eureka.client.model.entity.Response;
 import com.eureka.client.support.utils.RedisUtil;
@@ -16,14 +20,11 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.*;
 
 /**
  * @author techoneduan
@@ -36,27 +37,51 @@ public class ServiceClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(ServiceClient.class);
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate() {
+        {
+            setInterceptors(Collections.singletonList(new RestRequestInterceptor()));
+        }
+    };
 
     @Autowired
     private RedisUtil redisUtil;
 
     public Response call (String serviceName, String params) {
-        String reqAddr = buildRequestURL(serviceName);
-        ResponseEntity<String> resp = restTemplate.getForEntity(reqAddr, String.class);
-        return ResponseBuilder.build(true, resp.getBody());
+        String response = excuteCall(serviceName, params);
+        return ResponseBuilder.build(true, response);
     }
 
-    private String buildRequestURL (String serviceName) {
+    private String excuteCall (String serviceName, String params) {
         List<ServiceEntity> serviceList = pullServiceFromLocal();
         if (null == serviceList || serviceList.size() == 0)
             throw new UnifiedException(ErrorCodeEnum.NO_SERVICE_AVAILABLR);
+        /**
+         * TODO 超时重试、负载均衡
+         */
         Optional<ServiceEntity> optional = serviceList.stream().filter(item -> item.getServiceName().equals(serviceName)).findAny();
         if (null == optional || null == optional.get())
             throw new UnifiedException(ErrorCodeEnum.NO_SERVICE_AVAILABLR);
         ServiceEntity service = optional.get();
-        String reqAddr = "http://" + service.getIpAddr() + ":" + service.getPort() + service.getRouteAddr();
-        return reqAddr;
+        String resp = excuteByMethod(service, params);
+        return resp;
+    }
+
+    private String excuteByMethod (ServiceEntity service, String params) {
+        String resp = null;
+        String url = Constants.HTTP_PREFIX + service.getIpAddr() + Constants.PATH_SEPERATOR + service.getPort() + service.getRouteAddr();
+        params = Base64Util.encodeToUrlSafeString(params);
+        if (RestMethod.POST.equalsIgnoreCase(service.getMethod())) {
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, params, String.class);
+            resp = responseEntity.getBody();
+        } else if (RestMethod.GET.equalsIgnoreCase(service.getMethod())) {
+            /**
+             * 避免restTemplate强制url encode
+             */
+            UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url + Constants.QUESTION_MARK + params);
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(uriBuilder.build().toUri(), String.class);
+            resp = responseEntity.getBody();
+        }
+        return resp;
     }
 
     private List<ServiceEntity> pullServiceFromLocal () {
--- src/main/java/com/eureka/client/web/TestController.java
+++ src/main/java/com/eureka/client/web/TestController.java
@@ -1,12 +1,17 @@
 package com.eureka.client.web;
 
+import com.eureka.client.model.dto.SignInReqDTO;
 import com.eureka.client.support.bsp.ServiceClient;
 import com.eureka.client.support.annotation.ServiceInfo;
 import com.eureka.client.model.entity.Response;
+import com.eureka.client.support.utils.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author techoneduan
@@ -22,7 +27,10 @@ public class TestController {
     @RequestMapping(value = "/test")
     @ServiceInfo(name = "BLOG.TestController.test", description = "测试方法注册")
     public Response test () {
-        return caller.call("blogging.test", "");
+        SignInReqDTO reqDTO = new SignInReqDTO();
+        reqDTO.setRootPassword("tetetete");
+        reqDTO.setRootUserName("rererer");
+        return caller.call("blogging.test", JsonUtil.toString(reqDTO));
     }
 
     @RequestMapping(value = "/test2", method = RequestMethod.POST)
